1da87e59246b77f92629033c2930279f595548c4
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2023-04-06 02:51:22 +0600

    Registration logic added

--- register.php
+++ register.php
@@ -1,3 +1,63 @@
+<?php
+
+session_start();
+
+include('server/connection.php');
+
+if(isset($_POST['register'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $confirmPassword = $_POST['confirmPassword'];
+
+
+  if($password!==$confirmPassword){
+    header('location: register.php?error=Passwords do not match');
+  }
+
+  else if(strlen($password) < 6){
+    header('location: register.php?error=Passwords must be at least 6 characters');
+  }
+
+  else{
+    $stmt1 = $conn->prepare("SELECT count(*) FROM users WHERE user_email=?");
+    $stmt1->bind_param('s',$email);
+    $stmt1->execute();
+    $stmt1->bind_result($num_rows);
+    $stmt1->store_result();
+    $stmt1->fetch();
+
+    if($num_rows != 0){
+      header('location: register.php?error=User with this email already exists');
+    }
+    else{
+
+
+      $stmt = $conn->prepare("INSERT INTO users(user_name,user_email,user_password)
+      VALUES (?,?,?)");
+
+      $stmt->bind_param('sss',$name,$email,md5($password));
+      if($stmt->execute()){
+        $_SESSION['user_email']=$email;
+        $_SESSION['user_name']=$name;
+        $_SESSION['logged_in']=true;
+        header('location: account.php?register=You registered successfully');
+      }else{
+        header('location: register.php?error=Could not create account');
+      }
+    }
+  }
+}else if(isset($_SESSION['logged_in'])){
+  header('location: account.php');
+  exit;
+}
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -58,7 +118,8 @@
             <hr class="mx-auto">
         </div>
         <div class="mx-auto container">
-            <form id="register-form">
+            <form id="register-form" method="POST" action="register.php">
+              <p style="color: red;"><?php if(isset($_GET['error'])){ echo $_GET['error'];}?></p>
                 <div class="form-group">
                     <label>Name</label>
                     <input type="text" class="form-control" id="register-name" name="name" placeholder="Name" required />
@@ -76,10 +137,10 @@
                     <input type="password" class="form-control" id="register-confirm-password" name="confirmPassword" placeholder="Confirm Password" required />
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn" id="register-btn" value="Register"/>
+                    <input type="submit" class="btn" id="register-btn" name="register" value="Register"/>
                 </div>
                 <div class="form-group">
-                    <a id="login-url" class="btn">Do you have an account ? Login </a>
+                    <a id="login-url" href="login.php" class="btn">Do you have an account ? Login </a>
                 </div>
             </form>
         </div>
