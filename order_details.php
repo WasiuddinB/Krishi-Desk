1178b0931c02261d7d05e37b4a9734cdf6d89d2a
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2023-04-11 02:45:10 +0600

    Order Details and Details Page Added

--- order_details.php
+++ order_details.php
@@ -1,59 +1,24 @@
 <?php
 
-session_start();
 include('server/connection.php');
 
-if(!isset($_SESSION['logged_in'])){
-  header('location: login.php');
-  exit;
+if(isset($_POST['order_details_btn']) && isset($_POST['order_id'])){
+
+    $order_id=$_POST['order_id'];
+    $stmt=$conn->prepare("SELECT * FROM order_items WHERE order_id=?");
+    $stmt->bind_param('i',$order_id);
+    $stmt->execute();
+    $order_details=$stmt->get_result();
+
+}
+else{
+    header('location: account.php');
 }
 
-
-if(isset($_GET['logout'])){
-  if(isset($_SESSION['logged_in'])){
-    unset($_SESSION['logged_in']);
-    unset($_SESSION['user_email']);
-    unset($_SESSION['user_name']);
-    header('location: login.php');
-    exit;
-  }
-}
-
-
-if(isset($_POST['change_password'])){
-  $password=$_POST['password'];
-  $confirmPassword=$_POST['confirmPassword'];
-  $user_email=$_SESSION['user_email'];
-
-  if($password!==$confirmPassword){
-    header('location: account.php?error=Passwords do not match');
-  }
-
-  else if(strlen($password) < 6){
-    header('location: account.php?error=Passwords must be at least 6 characters');
-  }else{
-    $stmt=$conn->prepare("UPDATE users SET user_password=? WHERE user_email=?");
-    $stmt->bind_param('ss',md5($password),$user_email);
-
-    if($stmt->execute()){
-      header('location:  account.php?message=Passwords has been updated successfully');
-
-    }else{
-      header('location:  account.php?error=Could not update password');
-    }
-  }
-}
-
-
-if(isset($_SESSION['logged_in'])){
-  $user_id=$_SESSION['user_id'];
-  $stmt=$conn->prepare("SELECT * FROM orders WHERE user_id=?");
-  $stmt->bind_param('i',$user_id);
-  $stmt->execute();
-  $orders=$stmt->get_result();
-}
 
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -109,87 +74,41 @@ if(isset($_SESSION['logged_in'])){
     </nav>
 
 
-      <!--Account-->
-      <section class="my-5 py-5">
-        <div class="row container mx-auto">
-            <div class="text-center mt-3 pt-5 col-lg-6 col-md-12 col-sm-12">
-            <p class="text-center" style="color: green"><?php if(isset($_GET['register'])){echo $_GET['register'];} ?></p>
-                <h3 class="font-weight-bold">Account info</h3>
-                <hr class="mx-auto">
-                <div class="account-info">
-                    <p>Name <span><?php if(isset($_SESSION['user_name'])) {echo $_SESSION['user_name'];}?></span></p>
-                    <p>Email <span><?php if(isset($_SESSION['user_email'])) {echo $_SESSION['user_email'];}?></span></p>
-                    <p><a href="#orders" id="order-btn">Your Orders</a></p>
-                    <p><a href="account.php?logout=1" id="logout-btn">Logout</a></p>
-                </div>
-            </div>
-
-            <div class="col-lg-6 col-md-12 col-sm-12">
-                <form id="account-form" method="POST" action="account.php">
-                  <p class="text-center" style="color: red"><?php if(isset($_GET['error'])){echo $_GET['error'];} ?></p>
-                  <p class="text-center" style="color: green"><?php if(isset($_GET['message'])){echo $_GET['message'];} ?></p>
-                    <h3>Change Password</h3>
-                    <hr class="mx-auto">
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control" id="account-password" name="password" placeholder="Password" required/>
-                    </div>
-                    <div class="form-group">
-                        <label>Confirm Password</label>
-                        <input type="password" class="form-control" id="account-password-confirm" name="confirmPassword" placeholder="Confirm Password" required/>
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" value="Change Password" name="change_password" class="btn" id="change-pass-btn"/>
-                    </div>
-                </form>
-            </div>
-        </div>
-      </section>
-
-      <!--Orders-->
-        <section id="orders" class="orders container my-5 py-3">
-            <div class="container mt-2">
-                <h2 class="font-weight-bold text-center">Your Orders</h2>
+    <!--Order Details-->
+    <section id="orders" class="orders container my-5 py-3">
+            <div class="container mt-5">
+                <h2 class="font-weight-bold text-center">Order Details</h2>
                 <hr class="mx-auto">
             </div>
 
-            <table class="mt-5 pt-5">
+            <table class="mt-5 pt-5 mx-auto">
                 <tr>
-                    <th>Order ID: </th>
-                    <th>Order Cost: </th>
-                    <th>Order Status: </th>
-                    <th>Order Date: </th>
-                    <th>Order Details: </th>
+                    <th>Product: </th>
+                    <th>Product Price: </th>
+                    <th>Product Quantity: </th>
                 </tr>
 
-                <?php while($row=$orders->fetch_assoc()){ ?>
+                <?php while($row=$order_details->fetch_assoc()){ ?>
 
                         <tr>
                             <td>
-                                <!-- <div>
-                                    <img src="assets/imgs/back.jpg"/>
+                                <div>
+                                    <img src="assets/imgs/<?php echo $row['product_image'];  ?>"/>
                                     <div>
-                                        <p class="mt-3"><?php echo $row['order_id']; ?></p>
+                                        <p class="mt-3"><?php echo $row['product_name'];?></p>
                                     </div>
-                                </div> -->
-                                <span><?php echo $row['order_id']; ?></span>
+                                </div>
+                                
                             </td>
                             <td>
-                              <span><?php echo $row['order_cost']; ?></span>
+                              <span>Bdt.<?php echo $row['product_price'];?></span>
                             </td>
                             <td>
-                              <span><?php echo $row['order_status']; ?></span>
+                              <span><?php echo $row['product_quantity'];?></span>
                             </td>
-                            <td>
-                              <span><?php echo $row['order_date']; ?></span>
-                            </td>
+                            
 
-                            <td>
-                              <form method="POST" action="order_details.php">
-                                <input type="hidden" value="<?php echo $row['order_id']; ?>" name="order_id" />
-                                <input class="btn order-details-btn" name="order_details_btn" type="submit" value="Details" />
-                              </form>
-                            </td>
+                            
                             
                         </tr>
                     <?php } ?>
@@ -203,8 +122,8 @@ if(isset($_SESSION['logged_in'])){
 
 
 
-      <!--Footer-->
-      <footer class="mt-5 py-5">
+    <!--Footer-->
+    <footer class="mt-5 py-5">
         <div class="row container mx-auto pt-5">
           <div class="footer-one col-lg-3 col-md-6 col-sm-12">
             <img src="assets/imgs/logo22.png"/>
@@ -265,6 +184,7 @@ if(isset($_SESSION['logged_in'])){
 
 
 
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -43,6 +43,10 @@ if(isset($_POST['register'])){
 
       $stmt->bind_param('sss',$name,$email,md5($password));
       if($stmt->execute()){
+        $user_id=$stmt->insert_id;
+        $_SESSION['user_id']=$user_id;
+
+
         $_SESSION['user_email']=$email;
         $_SESSION['user_name']=$name;
         $_SESSION['logged_in']=true;
